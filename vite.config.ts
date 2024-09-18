b06606e5f9d56c704b00680ffae75ae50a96c78e
romelcumare <romel.cumare@solidsolutions.co.uk>
2024-09-18 13:21:39 +0100

    removed references to V2

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/SwoodDocsV2/"
+	plugins: [react()],
+	base: '/SwoodDocs/',
 })
