49965de2ce6372132f188e078dccbe9fde1b229c
SolidSolutionsLtd <romel.cumare@solidsolutions.co.uk>
2024-10-25 08:44:51 +0100

    Updates

---
+++ assets/sampleData-BptXknro.js
@@ -0,0 +1,1 @@
+const e=[{ReportVersion:"v2.10.3",ReleaseDate:"13/09/2024",SwoodVersion:"Swood 2024 SP0.0"},{ReportVersion:"v2.10.2",ReleaseDate:"6/09/2024",SwoodVersion:"Swood 2024 SP0.0"},{ReportVersion:"v2.10.1",ReleaseDate:"1/09/2024",SwoodVersion:"Swood 2024 SP0.0"},{ReportVersion:"v2.10.0",ReleaseDate:"20/08/2024",SwoodVersion:"Swood 2024 SP0.0"}];export{e as t};
