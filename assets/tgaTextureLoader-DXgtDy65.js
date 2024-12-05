3747efb0e6815690fe3703c49aca3ffc495853e5
SolidSolutionsLtd <romel.cumare@solidsolutions.co.uk>
2024-12-05 09:43:30 +0000

    Updates

---
+++ assets/tgaTextureLoader-DXgtDy65.js
@@ -0,0 +1,1 @@
+import{f as a,g as n}from"./BabylonPage-CISqWwnT.js";import"./index-DuQRaGW8.js";import"./useSlotProps-CaWmJsD_.js";class h{constructor(){this.supportCascades=!1}loadCubeData(){throw".env not supported in Cube."}loadData(e,t,s){const o=new Uint8Array(e.buffer,e.byteOffset,e.byteLength),r=a(o);s(r.width,r.height,t.generateMipMaps,!1,()=>{n(t,o)})}}export{h as _TGATextureLoader};
