17d471ca39787d7a68f1d897e98682db717e50ac
SolidSolutionsLtd <romel.cumare@solidsolutions.co.uk>
2024-12-19 10:51:47 +0000

    changes to news

--- src/components/LatestNews.tsx
+++ src/components/LatestNews.tsx
@@ -6,7 +6,7 @@ const LatestNews = () => {
 		<>
 			<Section id="LatestNews" title="Latest news...">
 				<Typography>
-					Swood 2024 SP3.0 is now available! To request a download link, please contact us at <a href="mailto:Support@SolidSolutions.co.uk">Support@SolidSolutions.co.uk</a>. This version is now compatible with SolidWorks 2025.
+					Swood 2024 SP3.0 is now available and fully compatible with SolidWorks 2025! To request a download link, please contact us at <a href="mailto:Support@SolidSolutions.co.uk">Support@SolidSolutions.co.uk</a>.
 				</Typography>
 			</Section>
 		</>
